7e0af118618516214b7cf4d4c107877152ef8ccc
Xavi Ivars <xavi.ivars@gmail.com>
2018-06-03 14:27:12 +0200

    Fail on error

--- dev/test-translations.sh
+++ dev/test-translations.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+set -e
+set -o pipefail
+
 cat test-sentences-spa.txt | apertium -d .. spa-cat > test-sentences-spa-cat.txt
 cat test-sentences-spa.txt | apertium -d .. spa-cat_valencia > test-sentences-spa-cat_valencia.txt
 git diff
