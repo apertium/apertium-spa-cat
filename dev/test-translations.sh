73cd8cf4a9edcf0c63a36737ce34635a1c4f91d8
Xavi Ivars <xavi.ivars@gmail.com>
2018-06-03 20:48:19 +0200

    Improvements on tests-script

--- dev/test-translations.sh
+++ dev/test-translations.sh
@@ -3,6 +3,9 @@
 set -e
 set -o pipefail
 
+echo "Translating test sentences"
 cat test-sentences-spa.txt | apertium -d .. spa-cat > test-sentences-spa-cat.txt
 cat test-sentences-spa.txt | apertium -d .. spa-cat_valencia > test-sentences-spa-cat_valencia.txt
-git diff
+
+echo "Checking for differences"
+git diff --exit-code
