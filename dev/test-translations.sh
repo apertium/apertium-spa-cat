a09376033222eaf95c66680b609b4e147d14f49c
Xavi Ivars <xavi.ivars@gmail.com>
2020-05-18 21:03:31 +0200

    Adding travis tests

--- dev/test-translations.sh
+++ dev/test-translations.sh
@@ -3,11 +3,15 @@
 #set -e
 #set -o pipefail
 
+pushd dev
+
 echo "Translating test sentences"
 cat test-sentences-spa.txt | apertium -d .. spa-cat > test-sentences-spa-cat.txt
 cat test-sentences-spa.txt | apertium -d .. spa-cat_valencia > test-sentences-spa-cat_valencia.txt
 
 ./test-grep.sh
+
+popd
 
 echo "Checking for differences"
 git diff --exit-code
