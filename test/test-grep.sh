ed9763c3b952887256a99a04de73e2b8a411c1af
Daniel Swanson <popcorn.tomato.dude@gmail.com>
2021-07-19 12:43:56 -0500

    convert existing tests to apertium-regtest

--- test/test-grep.sh
+++ test/test-grep.sh
@@ -4,7 +4,7 @@
 #set -o pipefail
 echo "Grep tests"
 {
-    file="../apertium-spa-cat.spa-cat.metadix"
+    file="apertium-spa-cat.spa-cat.metadix"
     grep -H "<i> " $file
     grep -H " </i>" $file
     grep -H "<l> " $file
@@ -22,7 +22,8 @@ echo "Grep tests"
     #Poden ser correctes
     grep -H "<b/></l>" $file #| sed -E '/n="(~|cont)/d'
     grep -H "<b/></r>" $file #| sed -E '/n="(~|cont)/d'
-    grep -H "<b/></i>" $file #| sed -E '/n="(~|cont|romana|.*el_qual__rel|.*el_seu__relecho "")/d' 
-} > "greptests.txt"
+    grep -H "<b/></i>" $file #| sed -E '/n="(~|cont|romana|.*el_qual__rel|.*el_seu__relecho "")/d'
+    exit 0
+}
 
 
