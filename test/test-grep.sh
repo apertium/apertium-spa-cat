383a3c648182fdf055155b13fdc94d2d5f69c3d6
Marc Riera <marc.riera.irigoyen@gmail.com>
2025-03-19 15:59:20 +0100

    Update tests

--- test/test-grep.sh
+++ test/test-grep.sh
@@ -4,26 +4,24 @@
 #set -o pipefail
 echo "Grep tests"
 {
-    file="apertium-spa-cat.spa-cat.metadix"
-    grep -H "<i> " $file
-    grep -H " </i>" $file
-    grep -H "<l> " $file
-    grep -H " </l>" $file
-    grep -H "<r> " $file
-    grep -H " </r>" $file
+for file in apertium-spa-cat.spa-cat.metadix
+do 
     grep -H -P "\xa0" $file # non-breaking space
-    grep -H "<b/><s" $file
-    grep -H "<b/><b/>" $file
-    grep -H "<b/><g>" $file
     grep -H -P "<i>[^<]+ [^<]+</i>" $file
     grep -H -P "<l>[^<]+ [^<]+</l>" $file
     grep -H -P "<r>[^<]+ [^<]+</r>" $file
+done
 
-    #Poden ser correctes
-    grep -H "<b/></l>" $file #| sed -E '/n="(~|cont)/d'
-    grep -H "<b/></r>" $file #| sed -E '/n="(~|cont)/d'
-    grep -H "<b/></i>" $file #| sed -E '/n="(~|cont|romana|.*el_qual__rel|.*el_seu__relecho "")/d'
-    exit 0
+for file in $(mktemp)
+do
+    lt-expand apertium-spa-cat.spa-cat.metadix > $file
+    grep " :" $file
+    grep ": " $file
+    grep " <" $file
+    grep "  " $file
+    grep " #" $file
+    grep -P "^ " $file
+    rm $file
+done
 }
-
 
